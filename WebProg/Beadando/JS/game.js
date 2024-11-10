38d1ad303c474fa026e53a399e0b656ead000441
J3NZ0L <jeneyzoltan2@gmail.com>
2024-11-10 17:29:25 +0100

    add Tile class with base logic, add baseEasyLevels and some const maps

--- WebProg/Beadando/JS/game.js
+++ WebProg/Beadando/JS/game.js
@@ -24,26 +24,186 @@ function toggleSelectedDifficulty(e){
 }
 
 function onStartButtonClick(e){
-    //toggleDisplay(gamePageDiv);
     toggleMenuAndGamePage()
-    createMatrix();
+    createStaticMatrix();
     initGame();
 }
 
+const tileTypes = ["emptyTile", "bridgeTile", "mountainTile", "oasisTile"]
+
+const railedTileTypes = {
+  "emptyTile": ["curve-rail", "straight-rail"],
+  "bridgeTile": "bridge-rail",
+  "mountainTile": "mountain-rail",
+}
+
+const possibleTravelDirectionMap = {
+}
+
+const Direction = Object.freeze({
+  NORTH: 0,
+  EAST: 1,
+  SOUTH: 2,
+  WEST: 3
+})
+
+const basePossibleDirections = {
+  "emptyTile": [0, 1, 2, 3],
+  "bridgeTile": [0, 2],
+  "mountainTile": [1, 2],
+  "oasisTile": [],
+}
+
 class Tile{
+  placeRail(){
+  }
 
+  rotateTile(){
+
+  }
+
+  rotateDirections( numOfTimes){
+    this.possibleEntrances.forEach(element => {
+      element=(element+numOfTimes)%4
+    });
+  }
+
+  incTravelDirection(){
+
+  }
+
+  isRailable(){
+    return true
+  }
+
+  getStyleClass(){
+  }
+}
+class EmptyTile extends Tile{
+  constructor(){
+    this.possibleEntrances = basePossibleDirections["emptyTile"]
+    this.possibleTravelDirection = basePossibleDirections["emptyTile"]
+    this.hasStraightRail = False
+    this.hasCurveRail = False
+
+    this.rotateDirections(numberOfRotations)
+  }
+
+  placeStraightRail(){
+    this.hasStraightRail = true
+  }
+
+  getStyleClass(){
+    if (this.hasStraightRail){
+      return "Tile-straight-rail"
+    } else if (this.hasCurveRail){
+      return "Tile-curve_rail"
+    } else {
+      return "Tile-empty"
+    }
+  }
+
+  canHaveStraightRail(){
+    return true
+  }
+
+  canHaveCurveRail(){
+    return true
+  }
 }
 
-function toggleDisplay(element) {
-    element.classList.toggle("hidden");
+class MountainTile extends Tile{
+  constructor(numberOfRotations){
+    this.possibleEntrances = basePossibleDirections["mountainTile"]
+    this.possibleTravelDirection = basePossibleDirections["mountainTile"]
+    this.numberOfRotations = numberOfRotations
+    this.hasRail = False
+
+    this.rotateDirections(numberOfRotations)
+  }
+
+  getStyleClass(){
+    return this.hasRail ? "Tile-mountain_rail": "Tile-mountain"
+  }
 }
+
+class BridgeTile extends Tile{
+  constructor(numberOfRotations){
+    this.possibleEntrances = basePossibleDirections["mountainTile"]
+    this.possibleTravelDirection = basePossibleDirections["mountainTile"]
+    this.numberOfRotations = numberOfRotations
+    this.hasRail = False
+
+    this.rotateDirections(numberOfRotations)
+  }
+
+  getStyleClass(){
+    return this.hasRail ? "Tile-bridge_rail" : "Tile-bridge"
+  }
+}
+
+class Oasis extends Tile{
+
+  isRailable(){
+    return false
+  }
+
+  getStyleClass(){
+    return "Tile-oasis"
+  }
+}
+
+const baseEasyLevels = [
+  [
+    [new EmptyTile(), new MountainTile(1), new EmptyTile(), new EmptyTile(), new Oasis()],
+    [new EmptyTile(), new EmptyTile(), new EmptyTile(), new BridgeTile(0), new Oasis()],
+    [new BridgeTile(), new EmptyTile(), new MountainTile(2), new EmptyTile(), new EmptyTile()],
+    [new EmptyTile(), new EmptyTile(), new EmptyTile(), new Oasis(), new EmptyTile()],
+    [new EmptyTile(), new EmptyTile(), new MountainTile(3), new EmptyTile(), new EmptyTile()]
+  ],
+  [
+    [new Oasis(), new EmptyTile(), new BridgeTile(3), new EmptyTile(), new EmptyTile()],
+    [new EmptyTile(), new MountainTile(2), new EmptyTile(), new EmptyTile(), new MountainTile(2)]
+    [new BridgeTile(0), new Oasis(), new MountainTile(3), new EmptyTile(), new EmptyTile()]
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(), new Oasis(),new EmptyTile()]
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile()]
+  ],
+  [
+    [new EmptyTile(),new EmptyTile(),new BridgeTile(3), new EmptyTile(),new EmptyTile()],
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile(),new BridgeTile(2)],
+    [new EmptyTile(), new MountainTile(2), new BridgeTile(0), new EmptyTile(),new EmptyTile()],
+    [new EmptyTile(), new Oasis(), new EmptyTile(),new EmptyTile(),new EmptyTile()],
+    [new EmptyTile(), new BridgeTile(3), new EmptyTile(),new EmptyTile(), new MountainTile(2)]
+  ],
+  [
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(), new BridgeTile(3), new EmptyTile(),]
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile()],
+    [new BridgeTile(0), new EmptyTile(), new MountainTile(2), new EmptyTile(), new MountainTile(2)],
+    [new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile(),new EmptyTile()],
+    [new EmptyTile(),new EmptyTile(), new Oasis(), new BridgeTile(3), new EmptyTile(),]
+  ],
+  [
+    [new EmptyTile(),new EmptyTile(), new BridgeTile(3), new EmptyTile(),new EmptyTile()],
+    [new EmptyTile(), new MountainTile(0), new EmptyTile(),new EmptyTile(),new EmptyTile()],
+    [new BridgeTile(0), new EmptyTile(),new EmptyTile(), new MountainTile(3), new EmptyTile(),],
+    [new EmptyTile(),new EmptyTile(), new BridgeTile(0), new Oasis(), new EmptyTile()],
+    [new EmptyTile(), new MountainTile(2), new EmptyTile(), new EmptyTile(),new EmptyTile()]
+    ]
+]
+
+
+function isCoordinateOfSide(x, y){
+  return x==0 || y==0 || x==size-1 || y==size-1
+}
+
 
 function toggleMenuAndGamePage(){
     menuPageDiv.classList.toggle("hidden");
     gamePageDiv.classList.toggle("hidden");
 }
 
-function createMatrix() {
+
+function createStaticMatrix() {
     const playfield = document.querySelector("#playfield");
     const size = parseInt(document.querySelector(".difficulty-button.selected").textContent.charAt(0))
     console.log(size)
@@ -56,7 +216,7 @@ function createMatrix() {
     // Generate cells for the matrix
     for (let i = 0; i < size * size; i++) {
       const cell = document.createElement("div");
-  
+      cell.classList.add("Tile-empty")
       // Optionally, set a background image for each cell
       //cell.style.backgroundImage = "url('path/to/image.png')";
   
@@ -86,7 +246,7 @@ function updateElapsedTime() {
   seconds = seconds < 10 ? '0' + seconds : seconds;
 
   // Update the displayed time
-  document.getElementById("elapsedTime").textContent = `${minutes}:${seconds}`;
+  document.querySelector("#elapsedTime").textContent = `${minutes}:${seconds}`;
 }
 
 // To stop the timer when the game ends (e.g., on game over):
